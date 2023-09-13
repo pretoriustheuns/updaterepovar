a3ad19c4d073962941c1a9d934c8b734cdd8a5c4
Theuns Pretorius <78522896+pretoriustheuns@users.noreply.github.com>
2023-09-13 16:18:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ NEWVALUE=$2
 OWNER=$3
 REPO=$4
 TOKEN=$5
-echo "v=$VARIABLE"
+echo "variable=$VARIABLE"
+echo "value=$NEWVALUE"
+echo "owner=$OWNER"
+echo "repo=$REPO"
+
+
 curl -L -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" https://api.github.com/repos/$OWNER/$REPO/actions/variables/$VARIABLE -d '{"name":"$VARIABLE","value":"$NEWVALUE"}'
