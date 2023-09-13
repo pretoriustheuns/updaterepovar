da9382b7668612613811280d2f39d3e80b068072
Theuns Pretorius <78522896+pretoriustheuns@users.noreply.github.com>
2023-09-13 15:06:45 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-curl -L -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer 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" https://api.github.com/repos/Signify-Software/Signify/actions/variables/MASTER_BUILDNOTES_LAST_COMMITSHA -d '{"name":"MASTER_BUILDNOTES_LAST_COMMITSHA","value":"updatefromgithub"}'
+curl -L -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $ACTIONS_RUNTIME_TOKEN" https://api.github.com/repos/Signify-Software/Signify/actions/variables/MASTER_BUILDNOTES_LAST_COMMITSHA -d '{"name":"MASTER_BUILDNOTES_LAST_COMMITSHA","value":"updatefromgithub"}'
