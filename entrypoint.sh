b152128e8ea3d49d23b398bb446d957353459fd1
Theuns Pretorius <78522896+pretoriustheuns@users.noreply.github.com>
2023-09-13 16:07:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ OWNER=$3
 REPO=$4
 TOKEN=$5
 
-curl -L -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" https://api.github.com/repos/$OWNER/$REPO/actions/variables/$$VARIABLE -d '{"name":"$VARIABLE","value":"$NEWVALUE"}'
+curl -L -X PATCH -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" https://api.github.com/repos/$OWNER/$REPO/actions/variables/$VARIABLE -d '{"name":"$VARIABLE","value":"$NEWVALUE"}'
